51ff88130cf275e2fb1d9f6f16bf88087213cb3a
Stawen DRONEK <stanislas.draunet@gmail.com>
2017-09-21 15:41:10 +0000

    create template

---
+++ v2/_templates/footer.php
@@ -0,0 +1,9 @@
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      <strong>MAIF &copy; 2017 </strong>
+    </div>
+    <!-- Default to the left -->
+     .....
+  </footer>
---
+++ v2/accueil-backend.php
@@ -0,0 +1,41 @@
+<?php
+	include_once '_templates/header.php';
+	include_once '_templates/menu.php';
+?> 
+  
+  
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Page Header
+        <small>Optional description</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+      <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+<?php
+	include_once '_templates/footer.php';
+	include_once '_templates/ctrlSideBar.php';
+	include_once '_templates/requiredJs.php';
+?> 
+
+ 
+
